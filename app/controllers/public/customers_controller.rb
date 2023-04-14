6d7f002014dc8fb54bd0d94462e40187ff543698
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-04-14 04:33:19 +0000

    interruption concert_evaluation

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -34,7 +34,7 @@ class Public::CustomersController < ApplicationController
 
   private
     def customer_params
-      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
+      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :nickname, :email, :postal_code, :address, :telephone_number)
     end
 
     def ensure_current_customer
