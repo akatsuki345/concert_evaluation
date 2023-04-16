a6fd17bbe346b6c68f1f807a126821988f02f21c
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-04-16 11:12:30 +0000

    interruption concert_evaluation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,7 +7,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:notice] = "ジャンルを追加しました！"
+      flash[:notice] = "カテゴリーを追加しました！"
       redirect_back(fallback_location: admin_categories_path)
     else
       @categories = Category.all
@@ -22,7 +22,7 @@ class Admin::CategoriesController < ApplicationController
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
-      flash[:notice] = "ジャンルを更新しました！"
+      flash[:notice] = "カテゴリーを更新しました！"
       redirect_to admin_categories_path(@category.id)
     else
       @categories = Category.all
