f43eeeebb02e2d747fcacbc5357b6115af8bd96d
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-04-17 03:08:50 +0000

    interruption concert_evaluation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,6 +17,7 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+  validates :nickname, presence: true
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true
