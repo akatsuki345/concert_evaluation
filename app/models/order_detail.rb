8059fbfb54bdc2f296dee87f52d5275a10bdbf30
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-04-18 06:56:05 +0000

    interruption concert_evaluation

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,7 +2,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :concert
   belongs_to :order
 
-  enum making_status: {hassouhuka: 0, hassoutyuu: 1, hassoukanryou: 2 }
+  enum making_status: {hassouhuka: 0, hassoujunbityuu: 1, hassoutyuu: 2, hassoukanryou: 3 }
   def add_total_payment_all
     tax = 1.1
     (self.concert.price * tax).floor * self.sheet
